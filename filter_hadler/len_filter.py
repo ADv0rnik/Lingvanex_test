2b2895a23479b4bae261400e3d497d9f58897cf2
Alex Dvornik <aadvornik@gmail.com>
2023-12-23 23:49:25 +0300

    Init commit

---
+++ filter_hadler/len_filter.py
@@ -0,0 +1,26 @@
+import math
+from opusfilter import FilterABC, CLEAN_BETWEEN
+
+
+class TenWordsLengthFilter(FilterABC):
+    score_direction = CLEAN_BETWEEN
+    accept_threshold = (0, math.inf)
+    reject_threshold = (math.inf, 0) 
+           
+    def __init__(self, max_length=10, pass_empty=False, **kwargs):
+        self.max_length = max_length
+        self.unit = 'word'
+        self.pass_empty = pass_empty
+        super().__init__(**kwargs)
+
+    def get_length(self, segment):
+        return len(segment.split())
+
+    def score(self, pairs):
+        for pair in pairs:
+            yield [self.get_length(segment) for segment in pair]
+
+    def accept(self, score):
+        if self.pass_empty and sum(score) == 0:
+            return True
+        return all(length <= self.max_length for length in score)
---
+++ filter_hadler/tests.py
@@ -0,0 +1,44 @@
+import unittest
+from len_filter import TenWordsLengthFilter
+from opusfilter.opusfilter import OpusFilter
+
+
+
+
+
+class TestTenWordsLengthFilter(unittest.TestCase):
+    
+    def setUp(self) -> None:
+        self.config = {
+            'steps': [
+                {
+                    'type': 'filter',
+                    'parameters': {
+                        'inputs': [
+                            'text.en-ru.en',
+                            'text.en-ru.ru'
+                        ],
+                        'outputs': [
+                            'text_f.en-ru.en',
+                            'text_f.en-ru.ru'
+                        ],
+                        'filters': [{
+                            'TenWordsLengthFilter': {
+                                'unit': 'word',
+                                'max_length': 10
+                            },
+                            'module': 'len_filter'
+                        }]
+                    }
+                }
+            ]
+        }
+        self.opus_filter = OpusFilter(self.config)
+        self.opus_filter.execute_steps()
+
+    def test_get_length(self):
+        test_string = "We need more heroines like you, Tina."
+        self.assertEqual(len(test_string), 37)
+        
+
+    
